23edee5c01891dd456e701adaa3869548272895b
whatshow <1257387641@qq.com>
2017-11-09 00:41:29 +0800

    添加示例代码

--- src/WifiManager.c
+++ src/WifiManager.c
@@ -71,80 +71,83 @@ void incorrentArguments(){
  * @out 输出绅辈
  * @okfn 函数指针，参数是 sk_buff * 类型。当所有的该HOOK点的所有登记函数调用完后，走此流程
  */
-//static unsigned int init_req(
-//        unsigned int hooknum,
-//        struct sk_buff * skb,
-//        const struct net_device *in,
-//        const struct net_device *out,
-//        int (*okfn) (struct sk_buff *)
-//)
-//{
-//    struct sk_buff *sk = skb;
-//    struct iphdr *ip;
-//    struct tcphdr *tcp;
-//    unsigned char *p_data;
-//    char http_GET[URL_MAX_LEN+2]={0};
-//    int n = 0;
-//    uint8_t *eth;
-//
-//    int len;
-//    union
-//    {
-//        unsigned int addr;
-//        unsigned char addr_ip[4];
-//    }ip_addr;
-//    //过滤空指针
-//    if (!sk)
-//        return NF_ACCEPT;
-//    //查看是否是IP数据包（排除ARP干扰）
-//    if(skb->protocol != htons(0x0800))
-//        return NF_ACCEPT;
-//
-//    eth = (uint8_t *)(skb->data) - 14;
-//    if(!eth)
-//    {
-//        printk("eth empty!\n");
-//        return NF_ACCEPT;
-//    }
-//
-//    memset(&msg, 0, sizeof(MY_HOST_URL_MSG));
-//    memcpy(msg.mac, eth + 6, 6);
-//
-//    ip = ip_hdr(sk);
-//    tcp = (struct tcphdr *)((unsigned char*)ip + ip->ihl*4);
-//    len = ntohs(ip->tot_len) - ip->ihl*4 - tcp->doff*4;
-//
-//    if(6 == ip->protocol)      //对TCP数据进行截获,http
-//    {
-//        if(80 != ntohs(tcp->dest))
-//            return NF_ACCEPT;
-//        p_data = (char *)tcp + tcp->doff*4; //数据开头
-//
-//        if(('G' == p_data[0]) && ('E' == p_data[1]))  //截获GET字段,这也是TCP分段第一段,http
-//        {
-//
-//            for(n = 0; n < ((len>URL_MAX_LEN)? URL_MAX_LEN:len); n++) //最多截取168个字符
-//            {
-//                http_GET[n] = p_data[n+4]; //剔除GET开头,提取url
-//
-//                if(0x20 == http_GET[n])
-//                {
-//                    http_GET[n] = '\0';
-//                    if(!strnicmp("HTTP/1.1",(char *)&(p_data[n+4+1]),strlen("HTTP/1.1")))
-//                    {
-//                        if(!exec_filter(p_data, len))
-//                        {
-//                            TCP_checksum(sk);
-//                            printk("change req ok!\n");
-//                        }
-//                        after_filter();
-//                        break;
-//                    }
-//                    break;
-//                }
-//            }
-//
-//        }
-//    }
-//    return NF_ACCEPT;
-//}
+static unsigned int init_req(
+        unsigned int hooknum,
+        struct sk_buff * skb,
+        const struct net_device *in,
+        const struct net_device *out,
+        int (*okfn) (struct sk_buff *)
+)
+{
+    struct sk_buff *sk = skb;
+    struct iphdr *ip;
+    struct tcphdr *tcp;
+    unsigned char *p_data;
+    char http_GET[URL_MAX_LEN+2]={0};
+    int n = 0;
+    uint8_t *eth;
+
+    int len;
+    union
+    {
+        unsigned int addr;
+        unsigned char addr_ip[4];
+    }ip_addr;
+    //过滤空指针
+    if (!sk)
+        return NF_ACCEPT;
+    //查看是否是IP数据包（排除ARP干扰）
+    if(skb->protocol != htons(0x0800))
+        return NF_ACCEPT;
+
+    eth = (uint8_t *)(skb->data) - 14;
+    if(!eth)
+    {
+        printk("eth empty!\n");
+        return NF_ACCEPT;
+    }
+
+    memset(&msg, 0, sizeof(MY_HOST_URL_MSG));
+    memcpy(msg.mac, eth + 6, 6);
+
+    //ip_hdr函数是作者自己定义的，返回 skb-> nh.iph
+    ip = ip_hdr(sk);
+    //这里假设机器是64位，char占4个字节，ip->ihl 表示ip头部存在多少个32bit
+    // 获取了tcp报文头部指针
+    tcp = (struct tcphdr *)((unsigned char*)ip + ip->ihl*4);
+    len = ntohs(ip->tot_len) - ip->ihl*4 - tcp->doff*4;
+
+    if(6 == ip->protocol)      //对TCP数据进行截获,http
+    {
+        if(80 != ntohs(tcp->dest))
+            return NF_ACCEPT;
+        p_data = (char *)tcp + tcp->doff*4; //数据开头
+
+        if(('G' == p_data[0]) && ('E' == p_data[1]))  //截获GET字段,这也是TCP分段第一段,http
+        {
+
+            for(n = 0; n < ((len>URL_MAX_LEN)? URL_MAX_LEN:len); n++) //最多截取168个字符
+            {
+                http_GET[n] = p_data[n+4]; //剔除GET开头,提取url
+
+                if(0x20 == http_GET[n])
+                {
+                    http_GET[n] = '\0';
+                    if(!strnicmp("HTTP/1.1",(char *)&(p_data[n+4+1]),strlen("HTTP/1.1")))
+                    {
+                        if(!exec_filter(p_data, len))
+                        {
+                            TCP_checksum(sk);
+                            printk("change req ok!\n");
+                        }
+                        after_filter();
+                        break;
+                    }
+                    break;
+                }
+            }
+
+        }
+    }
+    return NF_ACCEPT;
+}
